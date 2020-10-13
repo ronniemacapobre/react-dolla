f5472d831d717d6ae219cd5920bd40c5adbddab9
Ronaldo Macapobre <RonaldoM@magenic.com>
2020-10-14 00:30:51 +0800

    Initial commit with Nav and Side bar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import {
+  SidebarContainer,
+  CloseIcon,
+  Icon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SidebarBtnWrap,
+  SidebarRoute,
+} from './SidebarElements';
+
+const Sidebar = ({ isOpen, toggle }) => {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to='about' onClick={toggle}>
+            About
+          </SidebarLink>
+          <SidebarLink to='discover' onClick={toggle}>
+            Discover
+          </SidebarLink>
+          <SidebarLink to='services' onClick={toggle}>
+            Services
+          </SidebarLink>
+          <SidebarLink to='signup' onClick={toggle}>
+            Sign Up
+          </SidebarLink>
+        </SidebarMenu>
+        <SidebarBtnWrap>
+          <SidebarRoute to='/'>Sign In</SidebarRoute>
+        </SidebarBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+};
+
+export default Sidebar;

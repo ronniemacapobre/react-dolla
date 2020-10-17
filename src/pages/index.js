aa7ea20b6326760c944eb8ecc05054272b228ca7
Ronaldo Macapobre <RonaldoM@magenic.com>
2020-10-17 09:10:30 +0800

    Home page contents

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,14 @@ import React, { useState } from 'react';
 
 import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import InfoSection from '../components/InfoSection';
+import {
+  homeObjOne,
+  homeObjTwo,
+  homeObjThree,
+} from '../components/InfoSection/Data';
+import Services from '../components/Services';
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,6 +22,11 @@ const Home = () => {
     <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Navbar toggle={toggle} />
+      <HeroSection />
+      <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <Services />
+      <InfoSection {...homeObjThree} />
     </>
   );
 };

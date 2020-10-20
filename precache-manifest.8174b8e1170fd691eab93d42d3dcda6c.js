89bfc24b28c1aea69a749f0499421777e5819be9
Ronaldo Macapobre <RonaldoM@magenic.com>
2020-10-20 21:45:26 +0800

    Updates

---
+++ precache-manifest.8174b8e1170fd691eab93d42d3dcda6c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03bf86a8e57ee502e4ba79588ee73f51",
+    "url": "/react-mbtbank/index.html"
+  },
+  {
+    "revision": "e9c7db63fe205fa6c1b8",
+    "url": "/react-mbtbank/static/css/main.330aae2d.chunk.css"
+  },
+  {
+    "revision": "dddc19d156cf2cab65d1",
+    "url": "/react-mbtbank/static/js/2.3193bd48.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-mbtbank/static/js/2.3193bd48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c7db63fe205fa6c1b8",
+    "url": "/react-mbtbank/static/js/main.f0f2d8c4.chunk.js"
+  },
+  {
+    "revision": "547b506486cfff675576",
+    "url": "/react-mbtbank/static/js/runtime-main.a5c63fe4.js"
+  },
+  {
+    "revision": "2e4becb642e0e0262be689d9b7babb4d",
+    "url": "/react-mbtbank/static/media/svg-1.2e4becb6.svg"
+  },
+  {
+    "revision": "1db4594e4948e7fcb8d019b5384cf042",
+    "url": "/react-mbtbank/static/media/svg-2.1db4594e.svg"
+  },
+  {
+    "revision": "8041fe87bf8a1a8664c1ad23fe989dd8",
+    "url": "/react-mbtbank/static/media/svg-3.8041fe87.svg"
+  },
+  {
+    "revision": "b5a70d6c0f027ed09d63fb8268d954c2",
+    "url": "/react-mbtbank/static/media/video.b5a70d6c.mp4"
+  }
+]);
